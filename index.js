d6752d7bf873ca06288286bddb7efb68258509fc
Nemanja Ristic <rista.pirot@gmail.com>
2018-08-14 11:52:37 +0200

    added dirty property

--- index.js
+++ index.js
@@ -1,35 +1,53 @@
 var rValidate = new RValidate
 
+function alertSomething() {
+    rValidate.submitForm()
+}
+
 rValidate.addOption([
     {
         class: 'number',
         name: 'numberOne',
         required: true,
+        invalid: true,
+        dirty: false,
         minLength: 5
     }, {
         class: 'number',
         name: 'numberTwo',
+        invalid: true,
+        dirty: false,
         minLength: 2
     }, {
         class: 'number',
         name: 'numberThree',
+        invalid: true,
+        dirty: false,
         minLength: 9
     }, {
         class: 'number',
         name: 'numberFour',
+        invalid: true,
+        dirty: false,
         required: true,
         minLength: 4
     }, {
         class: 'email',
         name: 'emailOne',
+        invalid: true,
+        dirty: false,
         required: true
     }, {
         class: 'email',
         name: 'emailTwo',
+        invalid: true,
+        dirty: false,
         required: false
     }, {
         class: 'password',
         name: 'passwordOne',
+        invalid: true,
+        dirty: false,
         required: true,
         minLength: 8,
         maxLength: 12
@@ -37,6 +55,8 @@ rValidate.addOption([
         class: 'c-password',
         name: 'c-passwordOne',
         passField: 'passwordOne',
+        invalid: true,
+        dirty: false,
         required: true
     }
 ])
--- r-validate.js
+++ r-validate.js
@@ -11,9 +11,24 @@ function RValidate() {
         var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(email);
     }
-    
 
     return {
+        submitForm: function() {
+            let result = true
+            for(let opt in options) {
+                if (options[opt].invalid == true) {
+                    document.getElementsByName(options[opt].name)[0].style.backgroundColor = 'lightcoral'
+                    console.log('Field ' + options[opt].name + ' is invalid')
+                    result = false
+                } else if (options[opt].invalid == false) {
+                    document.getElementsByName(options[opt].name)[0].style.backgroundColor = 'white'
+                }
+            }
+            if (result) {
+                alert('Form is successfully submitted')
+            }
+            
+        },
         addOption: function(opt) {
             options = []
             classes = []
@@ -51,12 +66,19 @@ function RValidate() {
                                 var findOption = options.filter(el => el.name == this.name)
                                 switch(el) {
                                     case 'number': 
+                                        this.addEventListener('focusin', function() {
+                                            findOption[0].dirty = true
+                                        })
                                         this.addEventListener('input', function(event) {
                                             if (isNaN(event.target.value) || (event.target.value.length < findOption[0].minLength && event.target.value.length > 0)) {
                                                 this.style.backgroundColor = 'lightcoral'
+                                                findOption[0].invalid = true
                                                 console.log(findOption[0].minLength)
                                             } else {
                                                 this.style.backgroundColor = 'white'
+                                                if (findOption[0].dirty) {
+                                                    findOption[0].invalid = false
+                                                }
                                             }
                                         })
                                         this.addEventListener('focusout', function(event) {
@@ -64,47 +86,71 @@ function RValidate() {
                                             if (findOption) {
                                                 if (findOption[0].required && event.target.value == '') {
                                                     this.style.backgroundColor = 'lightcoral'
+                                                    findOption[0].invalid = true
                                                     console.log('The field ' + findOption[0].name + ' is required') 
                                                 }
                                             }
                                         })
                                         break;
                                     case 'email':
+                                        this.addEventListener('focusin', function() {
+                                            findOption[0].dirty = true
+                                        })
                                         this.addEventListener('input', function(event) {
                                             if (!validateEmail(event.target.value)) {
                                                 this.style.backgroundColor = 'lightcoral'
+                                                findOption[0].invalid = true
                                             } else {
                                                 this.style.backgroundColor = 'white'
+                                                if (findOption[0].dirty) {
+                                                    findOption[0].invalid = false
+                                                }
                                             }
                                         })
                                         this.addEventListener('focusout', function(event) {
                                             if (findOption) {
                                                 if (findOption[0].required && event.target.value == '') {
                                                     this.style.backgroundColor = 'lightcoral'
+                                                    findOption[0].invalid = true
                                                     console.log('The field ' + findOption[0].name + ' is required') 
                                                 }
                                             }
                                         })
                                         break;
                                     case 'password': 
+                                        this.addEventListener('focusin', function() {
+                                            findOption[0].dirty = true
+                                        })
                                         this.addEventListener('input', function(event) {
                                             if (findOption[0].maxLength && findOption[0].minLength) {
                                                 if (event.target.value.length < findOption[0].minLength || event.target.value.length > findOption[0].maxLength) {
                                                     this.style.backgroundColor = 'lightcoral'
+                                                    findOption[0].invalid = true
                                                 } else {
                                                     this.style.backgroundColor = 'white'
+                                                    if (findOption[0].dirty) {
+                                                        findOption[0].invalid = false
+                                                    }
                                                 }
                                             } else if (findOption[0].maxLength) {
                                                 if (event.target.value.length < 0 || event.target.value.length > findOption[0].maxLength) {
                                                     this.style.backgroundColor = 'lightcoral'
+                                                    findOption[0].invalid = true
                                                 } else {
                                                     this.style.backgroundColor = 'white'
+                                                    if (findOption[0].dirty) {
+                                                        findOption[0].invalid = false
+                                                    }
                                                 }
                                             } else if (findOption[0].minLength) {
                                                 if (event.target.value.length < findOption[0].minLength || event.target.value.length > 100) {
                                                     this.style.backgroundColor = 'lightcoral'
+                                                    findOption[0].invalid = true
                                                 } else {
                                                     this.style.backgroundColor = 'white'
+                                                    if (findOption[0].dirty) {
+                                                        findOption[0].invalid = false
+                                                    }
                                                 }
                                             }
                                                
@@ -113,18 +159,26 @@ function RValidate() {
                                             if (findOption) {
                                                 if (findOption[0].required && event.target.value == '') {
                                                     this.style.backgroundColor = 'lightcoral'
+                                                    findOption[0].invalid = true
                                                     console.log('The field ' + findOption[0].name + ' is required') 
                                                 }
                                             }
                                         })
                                         break;
                                     case 'c-password': 
+                                        this.addEventListener('focusin', function() {
+                                            findOption[0].dirty = true
+                                        })
                                         this.addEventListener('input', function(event) {
                                             if (findOption) {
                                                 if (document.getElementsByName(findOption[0].passField)[0].value !== event.target.value) {
                                                     this.style.backgroundColor = 'lightcoral'
+                                                    findOption[0].invalid = true
                                                 } else {
                                                     this.style.backgroundColor = 'white'
+                                                    if (findOption[0].dirty) {
+                                                        findOption[0].invalid = false
+                                                    }
                                                 }
                                             }
                                         })
